4b2d873d7dab20a44ef0b17cfbb712906e082f1b
Armando <roque510@hotmail.com>
2016-05-31 14:57:39 -0600

    fix refresh modulos Damian

--- Modulos/delete_seguridad.modulo.php
+++ Modulos/delete_seguridad.modulo.php
@@ -2,26 +2,18 @@
 require_once ('medoo.php');
 require_once('funciones.php');
 require_once('config.php');
-
 global $database;
-
-	echo "<b>usuario_alias:</b> ".$_GET["usuarioid"] ."<br>";
+	
  	$usuario_alias = $_GET["usuarioid"];
-
  	$datas = $database->select("usuarios",["usuarios.usuario_id"],["usuarios.usuario_alias" => $usuario_alias]);
 	$id = 0;
 	foreach($datas as $data)
 	{
 		$id = $data["usuario_id"]."<br/>";
 	}
-
-	echo "<b>usuario_id</b>: ".$id."<br>";
-
+	
 	$database->delete("usuarios_cias", ["usuario_id" => $id]);
 	$database->delete("usuarios_grupos", ["usuario_id" => $id]);
 	$database->delete("usuarios", ["usuario_id" => $id]);
-	
-
-	header('Location:.?mod=mant_seguridad');
-
+echo "<script>window.location.href = '../?mod=mant_seguridad';</script>";
 ?>
--- Modulos/update_seguridad.php
+++ Modulos/update_seguridad.php
@@ -1,52 +1,32 @@
 <?php
 require_once ('../medoo.php');
-require_once('../funciones.php');
 require_once('../config.php');
-
 global $database;
-
-	echo "<b>usuario_nombre:</b> ".$_POST["usuario_nombres"] ."<br>";
- 	$usuario_nombres = $_POST["usuario_nombres"];
-	echo "<b>usuario_apellido</b>: ".$_POST["usuario_apellidos"] ."<br>";
-	$usuario_apellidos = $_POST["usuario_apellidos"];
-	
-	echo "<b>usuario_alias:</b> ".$_POST["usuario_alias"] ."<br>";
+ 	$usuario_nombres = $_POST["usuario_nombres"];	
+	$usuario_apellidos = $_POST["usuario_apellidos"];		
 	$usuario_alias = $_POST["usuario_alias"];
-
 	$datas = $database->select("usuarios",["usuarios.usuario_id"],["usuarios.usuario_alias" => $usuario_alias]);
 	$id = 0;
 	foreach($datas as $data)
 	{
-		echo '<b>usuario_id:</b> '.$id = $data["usuario_id"]."<br/>";
-	}
-
-	echo "<b>usuario_password:</b> ".$_POST["usuario_password"] ."<br>";	
-	
+		$id = $data["usuario_id"];
+	}	
 	if ($_POST["usuario_password"]<>"**********") {
-		$usuario_password = md5($_POST["usuario_password"]);
-		echo $usuario_password;
-	}
-	echo "<b>usuario_cell:</b> ".$_POST["usuario_cell"] ."<br>";
+		$usuario_password = md5($_POST["usuario_password"]);	
+	}	
 	$usuario_cell = $_POST["usuario_cell"];
 	if (isset($_POST["usuario_telefono"])){
 		$usuario_telefono = $_POST["usuario_telefono"];	
 	}else{
 		$usuario_telefono = "0";
 	}
-	echo "<b>usuario_telefono:</b> ".$_POST["usuario_telefono"] ."<br>";
-	
-	echo "<b>usuario_estatus:</b> ".$_POST["usuario_estatus"] ."<br>";	
 	$usuario_estatus = $_POST["usuario_estatus"];
 	if (isset($_POST["usuario_isAdmin"])){
-		$usuario_isadmin = 1;
-		echo "<b>usuario_isAdmin:</b> ".$_POST["usuario_isAdmin"]."<br>";
+		$usuario_isadmin = 1;	
 	}
 	else{
-		$usuario_isadmin = 0;
-		echo "<b>usuario_isAdmin:</b> ".$_POST["usuario_isAdmin"]."</b><br>";
+		$usuario_isadmin = 0;		
 	}
-//recordar agregar el campo de la contraseña ya encritpada con laa nueva base de datos.
-	
 	if ($_POST["usuario_password"]<>"**********") {
 		$last_user_id = $database->update("usuarios",["usuarios.usuario_nombre" => strtoupper($usuario_nombres),
 												  "usuarios.usuario_apellido" => strtoupper($usuario_apellidos),
@@ -65,103 +45,29 @@ global $database;
 												  "usuarios.usuario_isadmin" => strtoupper($usuario_isadmin)],
 												  ["usuarios.usuario_id" => $id]);
 	}
-	/*
-		$last_user_id = $database->insert("usuarios",["usuarios.usuario_alias" => $usuario_alias,
-												  "usuarios.usuario_nombres" => $usuario_nombres,
-												  "usuarios.usuario_apellidos" => $usuario_apellidos,
-												  "usuarios.usuario_password" => $usuario_password,
-												  "usuarios.usuario_cel" => $usuario_cell,
-												  "usuarios.estatus_id" => $usuario_estatus,
-												  "usuarios.usuario_isadmin" => $usuario_isadmin]);
-	*/
-	//---------------Insert a tabla de usuarios -----------------------------
-	
-/*	var_dump($database->error()); 
-
-	if (is_null($database->error()[1]) && is_null($database->error()[2])){
-		echo "CORRECTO<br><br>";
-	}else{
-		echo "ERROR<br><br><br>";
-	}
-*/
-
-	$database->delete("usuarios_cias", ["usuario_id" => $id ]);
-
-	echo "<br><b>usuario_cia_id(multiples): </b><br>";
-
+	$database->delete("usuarios_cias", ["usuario_id" => $id ]);	
 	if (isset($_POST["usuario_cia_id"])){
-		foreach ($_POST["usuario_cia_id"] as $key => $value) {
-			echo $value."<br>";
+		foreach ($_POST["usuario_cia_id"] as $key => $value) {			
 			$last_user_id = $database->insert("usuarios_cias",["usuarios_cias.usuario_id" => $id,
 								  "usuarios_cias.cia_id" => $value]);
 	} 	
-	}else{
-		echo 'en blanco<br>';
-	}
-	
-	echo "<b>usuario_cia_id_asignadas:</b> <br>";
-
+	}else{		
+	}	
 	$compañias = 0;
 	if (isset($_POST["usuario_cia_id_asignadas"])){
-		foreach ($_POST["usuario_cia_id_asignadas"] as $key => $value) {
-			echo $value."<br>";
+		foreach ($_POST["usuario_cia_id_asignadas"] as $key => $value) {	
 			$compañias ++;
 		} 
-	}else{
-		echo 'en blanco<br>';	
-	}
-
-	echo '<b>Grupos Asingados:</b><br>';
-
+	}else{		
+	}	
 	$database->delete("usuarios_grupos", ["usuario_id" => $id ]);
-
 	$grupos=0;
 	if (isset($_POST["grupo_id_asignados"])){
-		foreach ($_POST["grupo_id_asignados"] as $key => $value) {
-			echo $value."<br>";
+		foreach ($_POST["grupo_id_asignados"] as $key => $value) {	
 			$grupos++;
 			$last_user_id = $database->insert("usuarios_grupos",["usuarios_grupos.usuario_id" => $id,
-												  "usuarios_grupos.grupo_id" => $value]);
-		} 
-	}else{
-		echo 'en blanco';
+												  "usuarios_grupos.grupo_id" => $value]);		} 
+	}else{		
 	}
-
-	header('Location:../?pg=mant_seguridad');
-
-	echo "<br><br><br>";
-	//echo $compañias ."<br>";
-	//echo $grupos;
-
-	/*$last_user_id = $database->insert("usuarios_cias",["usuarios_cia.usuario_id" => $usuario_alias,
-												  "usuarios_cia.cia_id" => $usuario_nombres]);
-*/
-
-	for ($i=0; $i < $compañias; $i++) { 
-		
-	}
-	
-
-	/*
-		$last_user_id = $database->insert("usuarios",["usuarios.usuario_alias" => $usuario_alias,
-												  "usuarios.usuario_nombres" => $usuario_nombres,
-												  "usuarios.usuario_apellidos" => $usuario_apellidos,
-												  "usuarios.usuario_password" => $usuario_password,
-												  "usuarios.usuario_cel" => $usuario_cell,
-												  "usuarios.estatus_id" => $usuario_estatus,
-												  "usuarios.usuario_isadmin" => $usuario_isadmin]);
-	*/
-	//---------------Insert a tabla de usuarios -----------------------------
-	
-	/*var_dump($database->error()); 
-
-	if (is_null($database->error()[1]) && is_null($database->error()[2])){
-		echo "CORRECTO<br><br>";
-	}else{
-		echo "ERROR<br><br><br>";
-	}
-*/
-
-
-
+@header('Location:../?pg=mant_seguridad');
 ?>
